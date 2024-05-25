e6b841ebe6ced32fb1ace82fe11e81ddfb8817f7
vishag002 <vvishag477@gmail.com>
2024-05-25 17:52:34 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hive_crud/view/home_screen.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('myBox');
   runApp(MyApp());
 }
 
