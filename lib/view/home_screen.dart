b74356baf9852343ff3ac78ab54bef3f94aa19db
vishag002 <vvishag477@gmail.com>
2024-05-25 17:52:12 +0530

    ui

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,124 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  //
-  final List<Map<String, String>> myList = [];
-  final nameController = TextEditingController();
-  final ageController = TextEditingController();
-  //save
-  void saveButton() {
-    setState(() {
-      myList.add({
-        'name': nameController.text,
-        'age': ageController.text,
-      });
-    });
-    ageController.clear();
-    nameController.clear();
-    print(myList.toString());
+  final TextEditingController keyController = TextEditingController();
+  final TextEditingController valueController = TextEditingController();
+  late Box myBox;
+
+  @override
+  void initState() {
+    super.initState();
+    myBox = Hive.box('myBox');
+  }
+
+  void addItem() {
+    final key = keyController.text;
+    final value = valueController.text;
+
+    if (key.isNotEmpty && value.isNotEmpty) {
+      myBox.put(key, value);
+
+      keyController.clear();
+      valueController.clear();
+
+      setState(() {});
+    }
+  }
+
+  void updateItem(String key, String value) {
+    keyController.text = key;
+    valueController.text = value;
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Update Item'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: keyController,
+              decoration: InputDecoration(labelText: 'Key'),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(labelText: 'Value'),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              final newKey = keyController.text;
+              final newValue = valueController.text;
+
+              if (newKey.isNotEmpty && newValue.isNotEmpty) {
+                myBox.delete(key);
+                myBox.put(newKey, newValue);
+
+                keyController.clear();
+                valueController.clear();
+
+                Navigator.of(context).pop();
+                setState(() {});
+              }
+            },
+            child: Text('Update'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void deleteItem(String key) {
+    myBox.delete(key);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text('Hive CRUD')),
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(25),
-            child: Container(
-              height: 250,
-              width: double.maxFinite,
-              //  color: Colors.amber,
-              child: ListView(
-                children: [
-                  ListTile(
-                    title: TextFormField(
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        alignLabelWithHint: true,
-                        hintText: "Name",
-                        border: UnderlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  ListTile(
-                    title: TextFormField(
-                      controller: ageController,
-                      decoration: InputDecoration(
-                        alignLabelWithHint: true,
-                        hintText: "Age",
-                        border: UnderlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.phone,
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  InkWell(
-                    onTap: saveButton,
-                    child: Container(
-                      height: 60,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: Colors.blue[100]),
-                      child: Center(
-                          child: Text(
-                        " Save to Hive",
-                        style: TextStyle(
-                            color: Colors.blue[900],
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500),
-                      )),
-                    ),
-                  )
-                ],
-              ),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: keyController,
+                  decoration: InputDecoration(labelText: 'Key'),
+                ),
+                TextField(
+                  controller: valueController,
+                  decoration: InputDecoration(labelText: 'Value'),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: addItem,
+                  child: Text('Add Item'),
+                ),
+              ],
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: myList.length,
-              addAutomaticKeepAlives: true,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  tileColor: Colors.blue[50],
-                  leading: InkWell(onTap: () {}, child: Icon(Icons.edit)),
-                  title: Text(myList[index]['name'].toString(),
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
-                  subtitle: Text(myList[index]['age'].toString(),
-                      style:
-                          TextStyle(fontSize: 17, fontWeight: FontWeight.w400)),
-                  trailing: InkWell(
-                    onTap: () {
-                      setState(() {
-                        print(myList.toString());
-                        myList.removeAt(index);
-                        print(myList.toString());
-                      });
-                    },
-                    child: Icon(
-                      Icons.delete,
-                      // color: Colors.black,
-                      size: 30,
-                    ),
-                  ),
-                ),
-              ),
+            child: ValueListenableBuilder(
+              valueListenable: myBox.listenable(),
+              builder: (context, Box box, _) {
+                if (box.isEmpty) {
+                  return Center(child: Text('No data available.'));
+                }
+
+                return ListView.builder(
+                  itemCount: box.length,
+                  itemBuilder: (context, index) {
+                    final key = box.keyAt(index);
+                    final value = box.get(key);
+
+                    if (key is String && value is String) {
+                      return ListTile(
+                        title: Text('Key: $key'),
+                        subtitle: Text('Value: $value'),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.edit),
+                              onPressed: () => updateItem(key, value),
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () => deleteItem(key),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      return Container(); // Handle non-string cases or errors
+                    }
+                  },
+                );
+              },
             ),
-          )
+          ),
         ],
       ),
     );
